0260536e19e567888c1cfd5983543028b5960ad4
Armchair Traveller <sentient.sober.armchair@gmail.com>
2024-10-04 17:26:56 -0700

    chore: typo nav New Releases link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,7 +9,6 @@ grouped semantically as a single header. -->
   <SuperHeader />
   <div class="align-center relative flex w-full items-center border-b border-b-gray-300 px-8 py-8">
     <Logo class="absolute top-1/2 -translate-y-1/2" />
-
     <nav class="flex w-full justify-center gap-8">
       {#each [['/sale', 'Sale'], ['/new', 'New Releases'], ['/men', 'Men'], ['/women', 'Women'], ['/kids', 'Kids'], ['/collections', 'Collections']] as [href, content]}
         <a class="text-lg font-medium uppercase text-gray-900 first-of-type:text-secondary" {href}
