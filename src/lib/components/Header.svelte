10573425288d59935312d68b07c2dc720a429a08
Armchair Traveller <sentient.sober.armchair@gmail.com>
2024-10-04 17:26:11 -0700

    exercise 2 solution

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,13 +7,14 @@ import SuperHeader from '$lib/components/SuperHeader.svelte'
 grouped semantically as a single header. -->
 <header>
   <SuperHeader />
-  <div class="border-b border-b-gray-300 px-8 py-0">
-    <Logo />
-    <nav class="">
-      {#each [['/sale', 'Sale'], ['/new', 'New&nbsp;Releases'], ['/men', 'Men'], ['/women', 'Women'], ['/kids', 'Kids'], ['/collections', 'Collections']] as [href, content]}
+  <div class="align-center relative flex w-full items-center border-b border-b-gray-300 px-8 py-8">
+    <Logo class="absolute top-1/2 -translate-y-1/2" />
+
+    <nav class="flex w-full justify-center gap-8">
+      {#each [['/sale', 'Sale'], ['/new', 'New Releases'], ['/men', 'Men'], ['/women', 'Women'], ['/kids', 'Kids'], ['/collections', 'Collections']] as [href, content]}
         <a class="text-lg font-medium uppercase text-gray-900 first-of-type:text-secondary" {href}
-          >{content}</a
-        >
+          >{content}
+        </a>
       {/each}
     </nav>
   </div>
