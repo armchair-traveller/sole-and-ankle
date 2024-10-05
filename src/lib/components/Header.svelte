3e69a7873cbb3c8510576a7bcda9ca4358767b84
Armchair Traveller <sentient.sober.armchair@gmail.com>
2024-10-04 17:26:56 -0700

    chore: typo nav New Releases link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,10 +10,10 @@ grouped semantically as a single header. -->
   <div class="border-b border-b-gray-300 px-8 py-0">
     <Logo />
     <nav class="">
-      {#each [['/sale', 'Sale'], ['/new', 'New&nbsp;Releases'], ['/men', 'Men'], ['/women', 'Women'], ['/kids', 'Kids'], ['/collections', 'Collections']] as [href, content]}
+      {#each [['/sale', 'Sale'], ['/new', 'New Releases'], ['/men', 'Men'], ['/women', 'Women'], ['/kids', 'Kids'], ['/collections', 'Collections']] as [href, content]}
         <a class="text-lg font-medium uppercase text-gray-900 first-of-type:text-secondary" {href}
-          >{content}</a
-        >
+          >{content}
+        </a>
       {/each}
     </nav>
   </div>
