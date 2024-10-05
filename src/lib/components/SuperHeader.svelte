2b01742e22c0b0a13f699c83e7b7dbcb5a7ed296
Armchair Traveller <sentient.sober.armchair@gmail.com>
2024-10-04 18:07:35 -0700

    fix: solution 1 superheader x padding too low

--- src/lib/components/SuperHeader.svelte
+++ src/lib/components/SuperHeader.svelte
@@ -4,7 +4,7 @@ import SearchInput from '$lib/components/SearchInput.svelte'
 import UnstyledButton from '$lib/components/UnstyledButton.svelte'
 </script>
 
-<div class="flex justify-between gap-4 bg-gray-900 px-6 py-2 text-sm text-gray-300">
+<div class="flex justify-between gap-4 bg-gray-900 px-8 py-2 text-sm text-gray-300">
   <span class="mr-auto text-white">Free shipping on domestic orders over $75!</span>
   <SearchInput />
   <a class="outline-offset-2 [&:not(:focus-visible)]:outline-none" href="/help">Help</a>
